68067d558d1d205bcaadb2eba45ae323404e4896
Davide "limaCAT" Inglima <limacat@gmail.com>
2021-01-04 17:28:52 +0100

    Spikes

--- Program.cs
+++ Program.cs
@@ -14,14 +14,51 @@
     You should have received a copy of the GNU Affero General Public License
     along with this program.  If not, see <https://www.gnu.org/licenses/>. */
 using System;
+using System.IO;
+using iText.Kernel.Pdf;
+using iText.Kernel.Font;
+using iText.Layout;
+using iText.IO.Font.Constants;
+using iText.Layout.Element;
 
 namespace DuckCalendar
 {
     class Program
     {
+        static readonly string target = @"D:\Temp\Astley.pdf";
+
+        private static void Hello()
+        {
+            var dest = new FileInfo(target);
+
+            var writer = new PdfWriter(dest);
+            var pdf = new PdfDocument(writer);
+            var document = new Document(pdf);
+            document.Add(new Paragraph("Hello World 2!"));
+            document.Close();
+        }
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var dest = new FileInfo(target);
+
+            var writer = new PdfWriter(dest);
+            var pdf = new PdfDocument(writer);
+            var document = new Document(pdf);
+
+            // Creating a PdfFont
+            var font = PdfFontFactory.CreateFont(StandardFonts.TIMES_ROMAN);
+            document.Add(new Paragraph("itext:").SetFont(font));
+
+            var list = new List().SetSymbolIndent(12).SetListSymbol("\u2022").SetFont(font);
+            list.Add(new ListItem("Never gonna give you up"))
+                .Add(new ListItem("Never gonna let you down"))
+                .Add(new ListItem("Never gonna run around and desert you"))
+                .Add(new ListItem("Never gonna make you cry"))
+                .Add(new ListItem("Never gonna say goodbye"))
+                .Add(new ListItem("Never gonna tell a lie and hurt you"));
+            document.Add(list);
+            document.Close();
         }
     }
 }
