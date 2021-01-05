1a190d16a7360b2d8a559476432dfa0924a301b2
Davide "limaCAT" Inglima <limacat@gmail.com>
2021-01-05 19:34:43 +0100

    Finished the tutorials, started hacking on the Calendar Implementation.

--- Program.cs
+++ Program.cs
@@ -15,19 +15,24 @@
     along with this program.  If not, see <https://www.gnu.org/licenses/>. */
 using System;
 using System.IO;
-using iText.Kernel.Pdf;
-using iText.Kernel.Font;
-using iText.Layout;
 using iText.IO.Font.Constants;
+using iText.IO.Image;
+using iText.IO.Util;
+using iText.Kernel.Colors;
+using iText.Kernel.Font;
+using iText.Kernel.Geom;
+using iText.Kernel.Pdf;
+using iText.Layout;
 using iText.Layout.Element;
+using iText.Layout.Properties;
 
 namespace DuckCalendar
 {
     class Program
     {
-        static readonly string target = @"D:\Temp\Astley.pdf";
+        // static readonly string target = @"D:\Temp\Astley.pdf";
 
-        private static void Hello()
+        private static void Hello(string target)
         {
             var dest = new FileInfo(target);
 
@@ -38,7 +43,7 @@ namespace DuckCalendar
             document.Close();
         }
 
-        static void Main(string[] args)
+        private static void Astley(string target)
         {
             var dest = new FileInfo(target);
 
@@ -59,6 +64,150 @@ namespace DuckCalendar
                 .Add(new ListItem("Never gonna tell a lie and hurt you"));
             document.Add(list);
             document.Close();
+        }
+
+        private static void FoxAndDog(string target, string dogf, string foxf)
+        {
+            var dest = new FileInfo(target);
+
+            var writer = new PdfWriter(dest);
+            var pdf = new PdfDocument(writer);
+            var document = new Document(pdf);
+
+            iText.Layout.Element.Image fox = new Image(ImageDataFactory.Create(foxf));
+            iText.Layout.Element.Image dog = new Image(ImageDataFactory.Create(dogf));
+            Paragraph p = new Paragraph("The quick brown ").Add(fox).Add(" jumps over the lazy ").Add(dog);
+            document.Add(p);
+            document.Close();
+        }
+
+        private static void DataTable(string target, string data)
+        {
+            var dest = new FileInfo(target);
+
+            var writer = new PdfWriter(dest);
+            var pdf = new PdfDocument(writer);
+            var document = new Document(pdf, PageSize.A4.Rotate());
+
+            document.SetMargins(20, 20, 20, 20);
+
+            var font = PdfFontFactory.CreateFont(StandardFonts.HELVETICA);
+            var bold = PdfFontFactory.CreateFont(StandardFonts.HELVETICA_BOLD);
+
+            var table = new Table(UnitValue.CreatePercentArray(new float[]
+            {
+                4, 1, 3, 4, 3, 3, 3, 3, 1 
+            })).UseAllAvailableWidth();
+
+            using (StreamReader sr = File.OpenText(data))
+            {
+                var line = sr.ReadLine();
+                Process(table, line, bold, true);
+                while ((line = sr.ReadLine()) != null)
+                {
+                    Process(table, line, font, false);
+                }
+            }
+
+            document.Add(table);
+            document.Close();
+        }
+
+        public static void Process(Table table, String line, PdfFont font, bool isHeader)
+        {
+            var tokenizer = new StringTokenizer(line, ";");
+            while (tokenizer.HasMoreTokens())
+            {
+                var cell = new Cell().Add(new Paragraph(tokenizer.NextToken()).SetFont(font));
+                if (isHeader)
+                {
+                    table.AddHeaderCell(cell);
+                } else
+                {
+                    table.AddCell(cell);
+                }
+            }
+        }
+
+        public struct Giorno
+        {
+            int numero;
+            string nome;
+
+            public Giorno(int numero, string nome)
+            {
+                this.numero = numero;
+                this.nome = nome;
+            }
+
+            public string Stampa()
+            {
+                if (numero != 0)
+                {
+                    return "" + numero + " " + nome;
+                } else
+                {
+                    return "";
+                }
+            }
+        }
+
+        private static void Calendar(string target)
+        {
+            var dest = new FileInfo(target);
+
+            var writer = new PdfWriter(dest);
+            var pdf = new PdfDocument(writer);
+            var document = new Document(pdf, PageSize.A4);
+
+            var cellHeight = (PageSize.A4.GetHeight() - 40) / 18;
+
+            document.SetMargins(20, 20, 20, 20);
+
+            var table = new Table(new float[2]).UseAllAvailableWidth();
+            table.SetMarginTop(0);
+            table.SetMarginBottom(0);
+
+            var rosso = new DeviceRgb(245, 15, 15);
+            var bianco = new DeviceRgb(255, 255, 255);
+
+            var cell = new Cell(2, 2).Add(new Paragraph("Gennaio"));
+            cell.SetTextAlignment(TextAlignment.CENTER);
+            cell.SetVerticalAlignment(VerticalAlignment.MIDDLE);
+            cell.SetFontColor(bianco);
+            cell.SetBackgroundColor(rosso);
+            // cell.SetHeight(cellHeight * 2);
+            table.AddCell(cell);
+
+            // popoliamo il mese
+            var mese = new Giorno[32];
+            // TODO vedere i mesi e quando inizia l'anno
+            for (int i = 1; i <= 31; i++)
+            {
+                mese[i - 1] = new Giorno(i, "Dormidì");
+            }
+            mese[31] = new Giorno(0, "");
+
+            for (int i = 0; i < 16; i++)
+            {
+                cell = new Cell().Add(new Paragraph(mese[i].Stampa())).SetHeight(cellHeight);
+                table.AddCell(cell);
+                cell = new Cell().Add(new Paragraph(mese[i + 16].Stampa())).SetHeight(cellHeight);
+                table.AddCell(cell);
+            }
+
+            document.Add(table);
+            document.Close();
+
+        }
+
+        static void Main(string[] args)
+        {
+            //Hello(@"D:\Temp\Hello.pdf");
+            // Astley(@"D:\Temp\Astley.pdf");
+            // FoxAndDog(@"D:\Temp\FoxAndDog.pdf", @"D:\Temp\dog.bmp", @"D:\Temp\fox.bmp");
+            // DataTable(@"D:\Temp\DataTable.pdf", @"D:\Temp\united_states.csv");
+            Calendar(@"D:\Temp\Gennaio.pdf");
         }
     }
 }
